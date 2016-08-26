810a1fce76b721e134b673c84e7bb96af4a88299
Max Bo <max@maxbo.me>
2016-08-26 23:39:40 +1000

    Fix performance issues and variable commenting

--- python/HyperCube.py
+++ python/HyperCube.py
@@ -1,19 +1,24 @@
 import cv2
 import sys
 
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(cascPath)
+cascade_path = sys.argv[1]
+classifier = cv2.CascadeClassifier(cascade_path)
 
+# https://realpython.com/blog/python/face-detection-in-python-using-a-webcam/
 video_capture = cv2.VideoCapture(0)
+
+# Scale the video down to size so as not to break performance
+video_capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640)
+video_capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 360)
 
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
+    faces = classifier.detectMultiScale(
+        gray_frame,
         scaleFactor=1.1,
         minNeighbors=2,
         minSize=(30, 30),
